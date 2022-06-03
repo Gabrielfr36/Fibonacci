21978110d77ef28e21a7995e6e5fcf3d9bd981a1
Gabriel Freire <55453002+Gabrielfr36@users.noreply.github.com>
2022-06-04 02:45:34 +0300

    Create Code .java

---
+++ Code .java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FibonacciSequence {
+
+    //////
+    ///Modify the example program to calculate F(n) by the given value of n.
+    //////
+
+    public static int fn(int n)
+    {
+        if (n <= 1) {
+            return n;
+        }
+
+        return fn(n - 1) + fn(n - 2);
+    }
+    public static void main(String[] args) {
+
+
+        System.out.println("FibonacciSequence:");
+        System.out.println("Enter the index of Fibonacci Sequence");
+
+        Scanner userInput = new Scanner(System.in);
+        int n = userInput.nextInt();
+
+        System.out.println("\nThe Fn of " + n + " : " + fn(n));
+
+    }
+}
